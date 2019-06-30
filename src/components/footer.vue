ed8b0284b8810aa421d797d3048e7a65836d70ed
杉田親次朗 <ba0534ps@icloud.com>
2019-06-30 14:37:25 +0900

    😆 footerを作った

---
+++ src/components/footer.vue
@@ -0,0 +1,13 @@
+<template>
+  <span id='footer'>
+    Sugita Shinjiro ☺︎
+  </span>
+</template>
+
+<style>
+#footer {
+  display: inline-block;
+  margin-top: 50px;
+  background: linear-gradient(transparent 60%, #00fa9a 60%);
+}
+</style>
